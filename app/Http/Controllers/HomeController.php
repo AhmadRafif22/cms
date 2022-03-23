063d9fea5adb3bf34444c6640733081e68d2e3e9
AhmadRafif22 <rafifayuzaki123@gmail.com>
2022-03-23 18:42:05 +0700

    jobsheet autentikasi - praktikum 3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +26,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        return view('home', ['user' => $user]);
     }
 }
